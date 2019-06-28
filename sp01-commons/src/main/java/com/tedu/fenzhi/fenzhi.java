a26f5aee66ea830d9475d0f0e394726dd25943b7
cczs920804 <ivers@DESKTOP-RUE756T>
2019-06-28 10:47:26 +0800

    123

--- sp01-commons/src/main/java/com/tedu/fenzhi/fenzhi.java
+++ sp01-commons/src/main/java/com/tedu/fenzhi/fenzhi.java
@@ -1,7 +1,8 @@
 package com.tedu.fenzhi;
 
 public class fenzhi {
-public static void main(String[] args) {
-	System.out.println("分支创建练习");
-}
+	
+	public static void main(String[] args) {
+		System.out.println("分支创建练习");	
+	}
 }
